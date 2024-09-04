a3c189c8fb886e6d7e931f201fd2fd2c325ece4e
Slava netologia <s-29@mail.ru>
2024-09-04 10:27:16 +0300

    Passed tests

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -1,13 +1,18 @@
 package ru.netology.testmode.test;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
 import static io.restassured.RestAssured.given;
 import static ru.netology.testmode.data.DataGenerator.*;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
@@ -30,6 +35,7 @@ class AuthTest {
         form.$("[name=login]").setValue(registeredUser.login);
         form.$("[name=password]").setValue(registeredUser.password);
         form.$(".button").click();
+        webdriver().shouldHave(url("http://localhost:9999/dashboard"));
         // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
         //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
         //  пользователя registeredUser
@@ -43,6 +49,8 @@ class AuthTest {
         form.$("[name=login]").setValue(notRegisteredUser.login);
         form.$("[name=password]").setValue(notRegisteredUser.password);
         form.$(".button").click();
+        $("[data-test-id=error-notification]").shouldBe(visible);
+        $("[data-test-id=error-notification]").shouldHave(text("Ошибка! Неверно указан логин или пароль"));
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
         //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
     }
@@ -55,6 +63,8 @@ class AuthTest {
         form.$("[name=login]").setValue(blockedUser.login);
         form.$("[name=password]").setValue(blockedUser.password);
         form.$(".button").click();
+        $("[data-test-id=error-notification]").shouldBe(visible);
+        $("[data-test-id=error-notification]").shouldHave(text("Ошибка! Пользователь заблокирован"));
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
         //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
     }
@@ -68,6 +78,8 @@ class AuthTest {
         form.$("[name=login]").setValue(wrongLogin);
         form.$("[name=password]").setValue(registeredUser.password);
         form.$(".button").click();
+        $("[data-test-id=error-notification]").shouldBe(visible);
+        $("[data-test-id=error-notification]").shouldHave(text("Ошибка! Неверно указан логин или пароль"));
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
         //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
         //  "Пароль" - пользователя registeredUser
@@ -82,6 +94,8 @@ class AuthTest {
         form.$("[name=login]").setValue(registeredUser.login);
         form.$("[name=password]").setValue(wrongPassword);
         form.$(".button").click();
+        $("[data-test-id=error-notification]").shouldBe(visible);
+        $("[data-test-id=error-notification]").shouldHave(text("Ошибка! Неверно указан логин или пароль"));
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
         //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
         //  "Пароль" - переменную wrongPassword
